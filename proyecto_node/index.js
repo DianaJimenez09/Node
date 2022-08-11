872950f58d4da6fc00ac0297be1be45f588f9bc1
DianaJimenez09 <djimenezneculman@gmail.com>
2022-08-11 16:51:56 +0200

    ultimo arreglo

--- proyecto_node/index.js
+++ proyecto_node/index.js
@@ -2,6 +2,10 @@ let myWrite = require("./writeAndReadObject");
 let myRead = require("./readConsole");
 
 console.log(
-  myWrite.writeAndRead("obj", { name: "joel", surname: "martinez", age: 40 })
+  myWrite.writeAndRead("./persona.json", {
+    name: "joel",
+    surname: "martinez",
+    age: 40,
+  })
 );
 console.log(myRead.readConsole("./persona.json"));
