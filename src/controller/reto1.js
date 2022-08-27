dbd16f988dd5506bfff0c85f74fbe938054e23c2
DianaJimenez09 <djimenezneculman@gmail.com>
2022-08-28 01:49:35 +0200

    .

---
+++ src/controller/reto1.js
@@ -0,0 +1,16 @@
+import { Professional } from "../../models/ImbdProfesional";
+
+let angelinaJolie = new Professional(
+  "Angelia Jolie",
+  50,
+  "female",
+  55,
+  1.73,
+  "brown",
+  "green",
+  "white-person",
+  false,
+  "american",
+  2,
+  "actress"
+);
