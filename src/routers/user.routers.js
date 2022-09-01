c283d7957808bc5dd99924be62c9ae44a4ecba5b
DianaJimenez09 <djimenezneculman@gmail.com>
2022-09-02 00:08:57 +0200

    reto 1y2

--- src/routers/user.routers.js
+++ src/routers/user.routers.js
@@ -1,12 +1,19 @@
 const { Router } = require("express");
 const router = Router();
-const userCtrl = require("../controller/user.controller");
+const professionalController = require("../controller/reto1");
+const profController = require("../controller/reto2");
 
-router.get("/", userCtrl.getStart);
-router.get("/usuario", userCtrl.getUser);
-router.get("/usuario/:id", userCtrl.getUser2parms);
-router.post("/usuario", userCtrl.postUser);
-router.put("/usuario", userCtrl.putUser);
-router.delete("/usuario", userCtrl.deleteUser);
+router.get("/", professionalController.getStart);
+router.get("/professional", professionalController.getProfessional);
+// router.get("/professional/:id", professionalController.getUser2parms);
+router.post("/professional", professionalController.postProfessional);
+router.put("/professional", professionalController.putProfessional);
+router.delete("/professional", professionalController.deleteProfessional);
+
+router.get("/", profController.getStart);
+router.get("/profes", profController.getProfes);
+router.get("/profes", profController.getProfesId);
+router.post("/profes", profController.postProfes);
+router.put("/profes", profController.putProfes);
 
 module.exports = router;
