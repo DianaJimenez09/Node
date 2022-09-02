2d71d81f080b77b4d0ba5cae4f251f99bb5e2798
DianaJimenez09 <djimenezneculman@gmail.com>
2022-09-02 12:41:12 +0200

    .m

--- src/routers/user.routers.js
+++ src/routers/user.routers.js
@@ -3,12 +3,12 @@ const router = Router();
 const professionalController = require("../controller/reto1");
 const profController = require("../controller/reto2");
 
-// router.get("/", professionalController.getStart);
-// router.get("/professional", professionalController.getProfessional);
-// // router.get("/professional/:id", professionalController.getUser2parms);
-// router.post("/professional", professionalController.postProfessional);
-// router.put("/professional", professionalController.putProfessional);
-// router.delete("/professional", professionalController.deleteProfessional);
+router.get("/", professionalController.getStart);
+router.get("/professional", professionalController.getProfessional);
+// router.get("/professional/:id", professionalController.getUser2parms);
+router.post("/professional", professionalController.postProfessional);
+router.put("/professional", professionalController.putProfessional);
+router.delete("/professional", professionalController.deleteProfessional);
 
 router.get("/", profController.getProfes);
 router.get("/profes", profController.getProfesId);
